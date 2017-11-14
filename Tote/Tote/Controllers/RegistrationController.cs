dbb096f287c365fb042b99f28757a6015fb1dc8a
CheryakVlad <cheryakv@mail.ru>
2017-11-14 14:30:55 +0300

    Tote-01 Add Simple View

--- Tote/Tote/Controllers/RegistrationController.cs
+++ Tote/Tote/Controllers/RegistrationController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tote.Models;
 
 namespace Tote.Controllers
 {
+
+    
+   
+
     public class RegistrationController : Controller
-    {
-        // GET: Registration
+    {        
+        List<Sport> sports = new List<Sport>() { new Sport() { Id = 1, Name = "Football" },
+        new Sport() { Id = 2, Name = "Hockey" }, new Sport() { Id = 3, Name = "Basketball" }};
+
         public ActionResult Index()
         {
-            return View();
+            return View(sports);
         }
     }
 }
