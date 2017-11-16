a17bd6ff4e4c8be0d85b36b7d7d53ba1ca928a47
CheryakVlad <cheryakv@mail.ru>
2017-11-16 22:19:49 +0300

    [Tote-02] Add attributes to class User

--- Tote/ToteBiz.Business/User.cs
+++ Tote/ToteBiz.Business/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,22 @@ using System.Threading.Tasks;
 namespace ToteBiz.Business
 {
     public class User
-    {
+    { 
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Login { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string RePassword { get; set; }
     }
 }
